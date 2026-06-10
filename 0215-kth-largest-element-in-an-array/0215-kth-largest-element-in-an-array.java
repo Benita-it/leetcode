51410bc58fec12f263326ade8fd12a21f09c08a2
Benita <thomasbenita18@gmail.com>
2026-06-10 11:53:56 +0530

    Time: 156 ms (5.17%), Space: 75.9 MB (5.01%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,18 +1,31 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        // int numsLen=nums.length;
+        int numsLen=nums.length;
         // Arrays.sort(nums); O(N*logn)
         // return nums[numsLen-k];
         //Since largest ele should be found use MaxHeap
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
-        for(int i=0;i<nums.length;i++){
-            pq.offer(nums[i]);
-        }
+        PriorityQueue<Integer> maxHeap=new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> minHeap=new PriorityQueue<>();
         int ans=0;
-        while(!pq.isEmpty() && k!=0){
-            ans=pq.peek();
-            pq.poll();
-            k=k-1;
+        if(k/2>numsLen/2){
+            for(int i=0;i<nums.length;i++){
+                minHeap.offer(nums[i]);
+            }
+            while(!minHeap.isEmpty() && k!=0){
+                ans=minHeap.peek();
+                minHeap.poll();
+                k--;
+            }
+        }
+        else{
+            for(int i=0;i<nums.length;i++){
+                maxHeap.offer(nums[i]);
+            }
+            while(!maxHeap.isEmpty() && k!=0){
+                ans=maxHeap.peek();
+                maxHeap.poll();
+                k--;
+            }
         }
         return ans;
     }
